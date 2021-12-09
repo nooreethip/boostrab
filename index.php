37539e790960ed39979536c1983264b6328ba3c7
nooreethip <86452749+nooreethip@users.noreply.github.com>
2021-12-09 13:58:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+        .block-1 {
+            max-width: 100%;
+            margin: 20px;
+            height: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="block-1"><img src="1.jpg"></div>
+        <div class="row">
+            <div class="col-6" height: auto>
+                <img src="2.jpg" class="img-fluid" />
+                <h1>ว่าด้วยเรื่องท้องฟ้า</h1>
+                
+
+            </div>
+
+            <div class="col-3">
+                <img src="https://i.pinimg.com/564x/a3/8b/bc/a38bbcc6e2f0f1088bc60d751e6baff4.jpg" class="img-fluid" />
+                
+            </div>
+            <div class="col-3">
+                <img src="https://i.pinimg.com/564x/e1/bd/24/e1bd24b31177ea49f42a7ba53da1e0af.jpg" class="img-fluid" />
+
+            </div>
+            
+                <p>รู้ใช่ปะว่าในแต่ละวันท้องฟ้าจะไม่เหมือนกันเลย
+
+                    วันนี้มีสีฟ้าสบายตา ไม่มีเมฆมาปกคลุมให้ดูรกสมอง
+                    เมื่อวานอาจดูหม่นหมอง เพราะมืดครึ้มคล้ายฝนจะตก
+                    และพรุ่งนี้อาจจะดูสดใส เนื่องจากมีแสงอาทิตย์มาเป็นจิตรกรระบายสีส้มอ่อนๆให้ดูสวยพอเป็นพิธี
+                    
+                    แต่เราว่าความสวยหรือไม่สวยของท้องฟ้าในแต่ละวัน มันขึ้นอยู่กับอารมณ์ ความรู้สึกของคนเรามากกว่า
+                    
+                    ถ้าเรามีความสุข ท้องฟ้าวันนั้นก็จะสวยเป็นพิเศษ แม้ว่าจะมืดฝนพายุจะถล่มก็ตาม
+                    
+                    แต่..
+                    
+                    ถ้าวันไหนมีความทุกข์ ท้องฟ้าก็จะกลายเป็นสีดำไปทันที ต่อให้วันนัั้นมันมีสีพาสเทลระบายอยู่ทั่วท้องฟ้าไปหมด
+                    
+                    ดังนั้น
+                    
+                    หากใครกำลังมีความทุกข์ พบเจอปัญหาโลกแตกที่ยังหาทางแก้ไม่ได้ มองทางไหนก็ไม่เห็นทางออก(อาจจะเห็นแต่ทางเข้า)
+                    
+                    เราอยากให้คุณลองพักสายตา(รวมถึงสมอง) ไปนั่งมองท้องฟ้าดูสักพักนะ ลองจ้องมันดูดีๆ ปล่อยสมองให้ว่างเปล่า ไล่ความคิดให้ไหลผ่านไปเรื่อยๆเหมือนที่เมฆเคลื่อนผ่านภูเขาอย่างเอื่อยช้า
+                    </p>
+        </div>
+
+</body>
+
+</html>
